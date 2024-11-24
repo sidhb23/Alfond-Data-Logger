e5cfa39462ab75dcde7d3578685352651d305350
Siddhartha <siddhartha.bajracharya@maine.edu>
2024-11-23 23:12:57 -0500

    added the Raspberry Pi script to the new main branch and also added README

---
+++ RaspberryPi Zigbee/receive_data.py
@@ -0,0 +1,11 @@
+import serial
+
+# Open serial connection to XBee connected via USB
+ser = serial.Serial('/dev/ttyUSB0', 57600, timeout=1)
+
+print("Waiting for data...")
+
+while True:
+    if ser.in_waiting > 0:
+        data = ser.readline().decode('utf-8').strip()
+        print(f"Received: {data}")
